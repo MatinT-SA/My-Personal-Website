b1107a5c023b8f21153c1d5ef9d20bc01ec675ee
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-04-28 13:45:01 +0330

    styles for the description text of stelix changed accordingly

--- dist/MatinTaherzadeh.9c33a697.js
+++ dist/MatinTaherzadeh.9c33a697.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1584" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1607" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
