0a1eab259c33fa374dde943b721822f51b68a8a4
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-05-18 16:36:46 +0330

    doing some update on about me

--- dist/MatinTaherzadeh.29775830.js
+++ dist/MatinTaherzadeh.29775830.js
@@ -627,7 +627,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1607" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7242" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
