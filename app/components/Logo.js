c5bc466c15d5b1e47b6159943087c50b79afd943
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-08-08 00:22:12 +0330

    refactoring header

--- app/components/Logo.js
+++ app/components/Logo.js
@@ -1,15 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
   return (
-    <a href="/">
+    <Link href="/" className="block w-full max-w-[192px] mx-auto">
       <Image
         src="/images/matin taherzade logo-02.webp"
         alt="Logo"
-        width={120}
-        height={60}
-        className="mx-auto"
+        width={192}
+        height={0}
+        className="w-full h-auto object-contain"
+        priority
       />
-    </a>
+    </Link>
   );
 }
