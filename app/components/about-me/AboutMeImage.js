bb5f75745ab479a8210c6aaebc431f7caa973051
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-08-22 21:06:05 +0330

    adding our own image as a fully transparent 3d image

--- app/components/about-me/AboutMeImage.js
+++ app/components/about-me/AboutMeImage.js
@@ -4,13 +4,13 @@ export default function AboutMeImage() {
   return (
     <div className="relative">
       <Image
-        src="/Content/images/bg2-com2.webp"
+        src="/images/matin-4.png"
         alt="computer image"
         width={534}
         height={399}
         quality={80}
         priority
-        className="block rounded-lg shadow-lg"
+        className="block rounded-lg"
       />
     </div>
   );
