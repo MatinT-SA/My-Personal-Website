541d873ccb41be2093c9fea155dbc3b9e54aefa6
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-09-07 15:00:00 +0330

    chaning resume to projects

---
+++ app/sections/Projects.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <section id="projects" className="h-96 scroll-mt-20">
+      Projects
+    </section>
+  );
+}
--- app/sections/Resume.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Resume() {
-  return (
-    <section id="resume" className="h-96 scroll-mt-20">
-      Resume
-    </section>
-  );
-}
