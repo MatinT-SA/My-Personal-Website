991a129ef9825a841ad8b6d9e9f733439e0f0414
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-09-19 19:00:00 +0330

    replacing our useless projects page link with the link to the projects page of our 3d portfolio

--- app/projects/page.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ProjectsPage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center text-white bg-gray-950 p-8">
-      <h1 className="text-4xl font-bold">صفحه پروژه ها!</h1>
-    </div>
-  );
-}
