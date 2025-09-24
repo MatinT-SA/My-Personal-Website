0832bd77b2c6c99bf6cbf6e27c6bf6a8727b8398
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-09-24 22:47:19 +0330

    refactoring contact card part

--- app/components/contact/ContactCard.js
+++ app/components/contact/ContactCard.js
@@ -11,17 +11,20 @@ export default function ContactCard({ icon: Icon, title, value, href }) {
       viewport={{ once: true, amount: 0.5 }}
       transition={{ duration: 0.5 }}
     >
-      <div className="absolute text-3xl text-blue-light transition-all duration-300 group-hover:scale-[4] group-hover:opacity-0">
+      <div className="absolute text-3xl text-blue-light transition-all duration-400 group-hover:scale-[4] group-hover:opacity-0">
         <Icon />
       </div>
-      <div className="card-content absolute inset-0 flex flex-col justify-center items-center text-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-        <h3 className="text-2xl text-blue-light font-bold mb-2 translate-y-[-25px] group-hover:translate-y-0 transition-transform duration-300">
+
+      <div className="card-content absolute inset-0 flex flex-col justify-center items-center text-center opacity-0 group-hover:opacity-100 transition-opacity duration-400 group-hover:delay-500">
+        <h3 className="text-md text-blue-light font-bold mb-2 translate-y-[-25px] group-hover:translate-y-0 transition-transform duration-300 group-hover:delay-500">
           {title}
         </h3>
-        <span className="text-yellow-primary text-xl font-medium translate-y-[10px] group-hover:translate-y-0 transition-transform duration-300">
+
+        <span className="text-yellow-primary text-sm font-medium translate-y-[10px] group-hover:translate-y-0 transition-transform duration-300 group-hover:delay-500">
           {value}
         </span>
       </div>
+
       {isLink && (
         <a
           href={href}
