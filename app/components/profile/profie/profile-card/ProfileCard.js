224ae9726793ee3c87900fb7e1d7f3ed15cbc935
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-12-13 18:04:48 +0330

    responsiveness - home section

--- app/components/profile/profie/profile-card/ProfileCard.js
+++ app/components/profile/profie/profile-card/ProfileCard.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ProfileButtons from "./ProfileButtons";
 import ProfileImage from "./ProfileImage";
 import ProfileInfo from "./ProfileInfo";
@@ -9,6 +9,7 @@ import SocialLinks from "./SocialLinks";
 
 export default function ProfileCard() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
   const profileImgRef = useRef();
 
   const getProfileRect = () => {
@@ -22,24 +23,43 @@ export default function ProfileCard() {
     setIsModalOpen(true);
   };
 
+  useEffect(() => {
+    const checkScreenWidth = () => {
+      setIsMobile(window.innerWidth < 550);
+    };
+
+    checkScreenWidth();
+    window.addEventListener("resize", checkScreenWidth);
+    return () => window.removeEventListener("resize", checkScreenWidth);
+  }, []);
+
   return (
     <div
-      className="relative w-full bg-[url('/images/profile-card-bg.svg')] bg-no-repeat bg-cover bg-bottom pt-1 pb-56 min-h-[71vh] animate-pic-load opacity-0"
+      className="relative w-full bg-[url('/images/profile-card-bg.svg')] bg-no-repeat bg-cover bg-bottom pt-1 pb-56 min-h-[71vh] animate-pic-load opacity-0 px-2"
       style={{
         backgroundColor: "var(--color-purple-secondary)",
         animationFillMode: "forwards",
       }}
     >
-      <div className="w-full max-w-sm mx-auto my-2 bg-purple-primary shadow-[0_10px_20px_-10px_rgba(0,0,0,0.75)] pt-1 pb-5 flex flex-col items-center relative rounded-md animate-float-profile">
+      <div
+        className={`w-full max-w-sm mx-auto my-2 bg-purple-primary shadow-[0_10px_20px_-10px_rgba(0,0,0,0.75)] pt-1 pb-5 flex flex-col items-center relative rounded-md ${
+          isMobile ? "" : "animate-float-profile"
+        }`}
+      >
         {/* Cover Photo */}
         <div
           className="w-full h-32 rounded-md -mb-24"
           style={{ backgroundColor: "var(--color-blue-light)" }}
         />
 
-        <ProfileImage onClick={handleProfileClick} refProp={profileImgRef} />
+        <ProfileImage
+          onClick={handleProfileClick}
+          refProp={profileImgRef}
+          isMobile={isMobile}
+          setIsMobile={setIsMobile}
+        />
 
-        <ProfileInfo />
+        <ProfileInfo isMobile={isMobile} setIsMobile={setIsMobile} />
 
         <ProfileButtons />
 
--- app/components/profile/profie/profile-card/ProfileInfo.js
+++ app/components/profile/profie/profile-card/ProfileInfo.js
@@ -1,14 +1,20 @@
 import { useTranslations } from "next-intl";
 
-export default function ProfileInfo() {
+export default function ProfileInfo({ isMobile, setIsMobile }) {
   const t = useTranslations("profile");
 
   return (
     <>
-      <div className="text-2xl font-bold text-white -mt-10 -ml-28">
+      <div
+        className={`text-2xl font-bold text-white ${
+          isMobile ? "mt-2" : "-mt-10 -ml-28"
+        }`}
+      >
         {t("info_name")}
       </div>
-      <div className="text-white mt-10 text-base">{t("info_role")}</div>
+      <div className={`text-white text-base ${isMobile ? "mt-1" : "mt-10"}`}>
+        {t("info_role")}
+      </div>
     </>
   );
 }
