6502b82f03933a6ef3c63423e639161cfb3b5d1c
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-12-14 23:49:30 +0330

    responsiveness - footer section

--- app/[locale]/sections/Footer.js
+++ app/[locale]/sections/Footer.js
@@ -6,7 +6,10 @@ import Button from "@/app/components/ui/Button";
 import Loader from "@/app/components/ui/Loader";
 import { SITE_VERSION } from "@/app/config";
 import { useTranslations } from "next-intl";
-import styles from "@/app/components/footer/footer.module.css";
+
+const customStyles = {
+  "--color-purple-primary": "var(--color-purple-primary)",
+};
 
 export default function Footer() {
   const t = useTranslations("footer");
@@ -38,7 +41,7 @@ export default function Footer() {
     const start = Date.now();
 
     if (href !== pathname) {
-      await router.push(href);
+      router.push(href);
     }
 
     const elapsed = Date.now() - start;
@@ -50,51 +53,63 @@ export default function Footer() {
   };
 
   return (
-    <div className={`${styles.footerOuter}`}>
+    <div className="relative overflow-hidden h-30">
       {loading && (
         <div className="fixed inset-0 z-9999 flex items-center justify-center bg-purple-primary/95 backdrop-blur-sm transition-opacity duration-200">
           <Loader />
         </div>
       )}
 
-      <div className={styles.footerShake}>
-        <footer className={styles.footerInner}>
-          <div className="text-md px-2 grid grid-cols-[1fr_2fr_1fr] text-center w-full items-center justify-center">
-            {/* Experience button */}
-            <Link href="/experience">
-              <Button
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleNavigation("/experience");
-                }}
-                className="bg-purple-primary text-blue-light border border-blue-light rounded-sm hover:bg-blue-light hover:text-purple-primary hover:border-purple-primary mx-auto"
-              >
-                {t("button_experience")}
-              </Button>
-            </Link>
+      <div className="w-full h-full group">
+        <div className="footer-shake-xl">
+          <footer
+            style={{ backgroundColor: customStyles["--color-purple-primary"] }}
+            className={`
+              relative text-white mx-auto w-[95%] max-w-7xl h-28 md:h-20 rounded-xl flex items-center tracking-wider text-center p-x-2 z-10 
+              transform translate-y-2
+              md:translate-y-6
+              xl:translate-y-24
+              transition-transform duration-400 ease-in-out
+              xl:group-hover:translate-y-4
+            `}
+          >
+            <div className="text-xs sm:text-sm md:text-base px-2 grid grid-cols-1 gap-1 xs:gap-2 md:grid-cols-[1fr_2.5fr_0.5fr] lg:grid-cols-[1fr_2fr_1fr] text-center w-full items-center justify-center">
+              {/* Experience button */}
+              <Link href="/experience">
+                <Button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleNavigation("/experience");
+                  }}
+                  className="bg-purple-primary text-blue-light border border-blue-light rounded-sm hover:bg-blue-light hover:text-purple-primary hover:border-purple-primary mx-auto"
+                >
+                  {t("button_experience")}
+                </Button>
+              </Link>
 
-            {/* Home link */}
-            <p>
-              © {new Date().getFullYear()} {t("rights_reserved")}{" "}
-              <Link
-                href="/"
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleNavigation("/");
-                }}
-                className="text-amber-100 font-bold transition-colors hover:text-yellow-primary tracking-wider cursor-pointer"
-              >
-                matintaherzadeh.ir
-              </Link>{" "}
-              {t("righs_reserved_rest")}
-            </p>
+              {/* Home link */}
+              <p>
+                © {new Date().getFullYear()} {t("rights_reserved")}{" "}
+                <Link
+                  href="/"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleNavigation("/");
+                  }}
+                  className="text-amber-100 font-bold transition-colors hover:text-yellow-primary tracking-wider cursor-pointer"
+                >
+                  matintaherzadeh.ir
+                </Link>{" "}
+                {t("righs_reserved_rest")}
+              </p>
 
-            {/* Version */}
-            <span className="text-sm font-mono opacity-70">
-              v{SITE_VERSION}
-            </span>
-          </div>
-        </footer>
+              {/* Version */}
+              <span className="text-sm font-mono opacity-70">
+                v{SITE_VERSION}
+              </span>
+            </div>
+          </footer>
+        </div>
       </div>
     </div>
   );
