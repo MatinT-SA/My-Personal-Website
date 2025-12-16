46487e8432092cfe660268c3535d8648b90562bd
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-12-16 22:46:36 +0330

    updating middlware.js for automatic language detection fix

--- middleware.js
+++ middleware.js
@@ -1,7 +1,13 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+export default createMiddleware({
+  locales: routing.locales,
+
+  defaultLocale: routing.defaultLocale,
+
+  localeDetection: false,
+});
 
 export const config = {
   matcher: ["/", "/(fa|en)/:path*"],
