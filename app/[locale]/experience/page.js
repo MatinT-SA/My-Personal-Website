1c34322503a9ff3283cc8d273d35d8bc19a2e235
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-12-17 19:17:32 +0330

    metadata for our experience page

--- app/[locale]/experience/page.js
+++ app/[locale]/experience/page.js
@@ -1,4 +1,23 @@
+import { getTranslations } from "next-intl/server";
 import ExperiencePageClient from "./ExperiencePageClient";
+
+export async function generateMetadata({ params: { locale } }) {
+  const t = await getTranslations({ locale, namespace: "seo.experience" });
+
+  return {
+    title: t("title"),
+    description: t("description"),
+
+    alternates: {
+      canonical: `https://matintaherzadeh.com/${locale}/experience`,
+      languages: {
+        fa: "https://matintaherzadeh.com/fa/experience",
+        en: "https://matintaherzadeh.com/en/experience",
+        "x-default": "https://matintaherzadeh.com/fa/experience",
+      },
+    },
+  };
+}
 
 export default function ExperiencePage() {
   return <ExperiencePageClient />;
