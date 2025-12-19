ffd4d3d896a3cd13a51e99269c035865382c5c92
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-12-19 23:44:53 +0330

    fixing gap after decomposing our resume downloader button for this part in our about me section

--- app/components/about-me/ResumeDownloadButton.js
+++ app/components/about-me/ResumeDownloadButton.js
@@ -58,7 +58,7 @@ export default function ResumeDownloadButton() {
       className="
         px-8 py-4 text-lg font-bold rounded-full
         text-blue-light bg-purple-primary
-        hover:text-purple-primary hover:bg-blue-300 hover:shadow-lg
+        hover:text-purple-primary hover:bg-blue-300 hover:shadow-lg hover:gap-3.5
       "
     >
       {t("button_resume")}
